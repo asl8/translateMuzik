32ff2f68edb1f529fdd3fe03655c565d7a902dea
nbosco <nbosco182@gmail.com>
2016-04-23 14:04:23 -0700

    Added oxford speech to text

--- MuzikTransl/platforms/ios/www/cordova_plugins.js
+++ MuzikTransl/platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.projectoxford.cordova.speechrecognition/www/oxfordspeechrecognition.js",
+        "id": "com.projectoxford.cordova.speechrecognition.OxfordSpeechRecognition",
+        "pluginId": "com.projectoxford.cordova.speechrecognition",
+        "clobbers": [
+            "window.OxfordSpeechRecognition"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "com.projectoxford.cordova.speechrecognition": "0.0.1"
 }
 // BOTTOM OF METADATA
 });
